0758a288aae23fe269111d8e36ed8fa7fb5c2f46
claronid17 <claronid17@mail.wlu.edu>
2016-05-13 11:28:59 -0400

    Add files via upload

---
+++ Claroni_Jones_Maze/bin/claroni_jones_maze/core_test.clj
@@ -0,0 +1,7 @@
+(ns claroni-jones-maze.core-test
+  (:require [clojure.test :refer :all]
+            [claroni-jones-maze.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
